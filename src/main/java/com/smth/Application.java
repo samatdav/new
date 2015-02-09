d90b0339dc9534ed5ac4acc2d17883de94c21978
devtur <khomenkoilya@gmail.com>
2015-02-09 23:32:41 +0300

    Reformat code

--- src/main/java/com/smth/Application.java
+++ src/main/java/com/smth/Application.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
-    ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 
+        ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
     }
 }
