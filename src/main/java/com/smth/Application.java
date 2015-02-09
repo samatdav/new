9e1feb65f6f22eae06210e1e6a23c6e5d7c1eeb7
devtur <khomenkoilya@gmail.com>
2015-02-09 23:31:47 +0300

    Added application class

---
+++ src/main/java/com/smth/Application.java
@@ -0,0 +1,21 @@
+package com.smth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author devtur.
+ */
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+    ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+
+    }
+}
