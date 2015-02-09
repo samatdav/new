309372d67f4d635f0730d23a1727f5ba27b5c4dd
Sam Dav <samatdav@gmail.com>
2015-02-09 17:37:50 +0300

    lightbox works

--- js/project.js
+++ js/project.js
@@ -22,27 +22,45 @@
     //   	$(".modal-profile").modal({show:true});
     // });
 
-
     // Establish the array which acts as a data source for the list
 var list = [
-{"type" : "buy", "img" : "img/sun.jpg", "title" : "BDS!", "text" : "wow so wow so wow", "price" : "999"},
-{"type" : "sell", "img" : "img/sun.jpg", "title" : "Hello!", "text" : "blah blah", "price" : "1000"},
-{"type" : "buy", "img" : "img/sun.jpg", "title" : "Hello!", "text" : "blah blah", "price" : "1000"},
-{"type" : "sell", "img" : "img/sun.jpg", "title" : "Hello!", "text" : "blah blah", "price" : "1000"}
+{"type" : "buy", "img" : "img/sun.jpg", "title" : "BDS!", "text" : "wow so wow so wow", "price" : "999", "longtext" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+{"type" : "sell", "img" : "img/sun.jpg", "title" : "Hello!", "text" : "blah blah", "price" : "1000", "longtext" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+{"type" : "buy", "img" : "img/sun.jpg", "title" : "Hello!", "text" : "blah blah", "price" : "1000", "longtext" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+{"type" : "sell", "img" : "img/sun.jpg", "title" : "Hello!", "text" : "blah blah", "price" : "1000", "longtext" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
 ];
 
 function makelist(){
 
-for( var i =  0 ; i < list.length; ++i){     
+for( var i =  0 ; i < list.length; ++i){
+	// displayable item 
+
+ //    var listContainer = document.createElement("div");
+ //    listContainer.className = "item";
+ //    document.querySelectorAll('.' + list[i].type)[0].appendChild(listContainer);
+	// $('.' + list[i].type).append(
+	// '<li class="various item col-md-12" onclick="location.href=#'+i+
+	// ';"><img class="image" src="' + list[i].img + 
+	// '" width="90" height="90"/><h2><a href="#' + i + 
+	// '">'+list[i].title+'</a></h3><p>'+ list[i].text +'</p><h2>' + list[i].price + 'p</h3></li>');
+
     var listContainer = document.createElement("div");
     listContainer.className = "item";
     document.querySelectorAll('.' + list[i].type)[0].appendChild(listContainer);
-
 	$('.' + list[i].type).append(
-	'<li class="item col-md-12" onclick="location.href=\'' + list[i].link + 
-	'\';"><img class="image" src="' + list[i].img + 
-	'" width="90" height="90"/><h2><a href="' + list[i].link + 
-	'">'+list[i].title+'</a></h3><p>'+ list[i].text +'</p><h2>' + list[i].price + 'p</h3></li>');
+	'<li onclick="location.href=#'+i+'"class="item col-md-12 various" href="#'+i+
+	'"><img class="image" src="' + list[i].img + 
+	'" width="90" height="90"/><h2><a href="#' + i + 
+	'">'+list[i].title+'</a></h2><p>'+ list[i].text +'</p><h3>' + list[i].price + 'p</h3></li>');
+
+	// after item was clicked
+	var listContainer = document.createElement("div");
+    listContainer.className = "clicked";
+    // document.querySelectorAll('.' + list[i].type)[0].appendChild(listContainer);
+
+	$('.clicked').append(
+	'<div id="'+i+'" class="box item col-md-12";"><img class="image" src="' + list[i].img + 
+	'"/><h1>'+list[i].title+'</h1><h2>'+ list[i].text +'</h2><h3>'+list[i].longtext+'</h3><h2>' + list[i].price + 'p</h2></div>');
     }
 }
 
